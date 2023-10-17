19fcf517d88ba565c9e923c2c4f56471b3304cd3
rizkifauzip <rizkipermana616@gmail.com>
2023-10-17 11:02:16 +0700

    PerubahanFile

--- Pertemuan2.js
+++ Pertemuan2.js
@@ -1,3 +1,2 @@
-console.log("Nama             : Rizki Fauzi Permana");
-console.log("Nama Panggilan   : Rizki / Fauzi");
-console.log("PELATIHAN CODING BATCH 7");
+var nama="Rizki Fauzi Permana";
+console.log(nama);
